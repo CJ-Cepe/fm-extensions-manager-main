fd033621140448b8d5f3e7b9195efbd882614c7b
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2025-11-14 23:18:07 +0800

    feat: settle changing theme logic

--- src/components/ThemeSwitch.jsx
+++ src/components/ThemeSwitch.jsx
@@ -1,6 +1,43 @@
-import lightModeIcon from "/assets/images/icon-sun.svg";
-import darkModeIcon from "/assets/images/icon-moon.svg";
+import iconSun from "/assets/images/icon-sun.svg";
+import iconMoon from "/assets/images/icon-moon.svg";
+import { useState, useEffect } from "react";
+
+const getInitialTheme = () => {
+  if (typeof window !== "undefined" && window.matchMedia) {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  }
+  return "light";
+};
 
 export default function ThemeSwitch() {
-  return <img src={darkModeIcon} alt="Theme Icon" />;
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  useEffect(() => {
+    const body = document.body;
+    body.classList.remove("light-override", "dark-override");
+    if (theme === "dark") {
+      body.classList.add("dark-override");
+    } else if (theme === "light") {
+      body.classList.add("light-override");
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <button aria-label="Toggle theme" onClick={toggleTheme}>
+      <img
+        src={theme === "light" ? iconMoon : iconSun}
+        alt={
+          theme === "light"
+            ? "Moon icon for dark theme"
+            : "Sun icon for light theme"
+        }
+      />
+    </button>
+  );
 }
