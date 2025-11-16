f92ae91f74a52e80167995bb64fae9f8ca0d43f6
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2025-11-16 23:40:09 +0800

    ci: add github deploy workflow and update vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/fm-extensions-manager-main/",
   server: {
     port: 3000, // optional: customize port
   },
